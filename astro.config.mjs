d410dea6c5ac1768ddd08da9cc0fbcd28b429412
dgddgd314 <dgddgd314@gmail.com>
2026-04-11 19:12:33 +0900

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'; // 최신 v4 방식
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dgddgd314.github.io',
+
+  integrations: [mdx()],
+
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
