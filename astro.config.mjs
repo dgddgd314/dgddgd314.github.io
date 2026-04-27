06f6c5ba3636715382fdf2ace1d87a70e4945f38
dgddgd314 <dgddgd314@gmail.com>
2026-04-28 01:21:12 +0900

    feat: upgraded into astro 6 based

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
