72d923c1d5be85d091b293afa4bb0b4358591a46
dgddgd314 <dgddgd314@gmail.com>
2026-04-28 01:04:21 +0900

    feat: applyed astrofy structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'; // 최신 v4 방식
 import mdx from '@astrojs/mdx';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://dgddgd314.github.io',
-
-  integrations: [mdx()],
-
-  markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://astrofy-template.netlify.app',
+  integrations: [mdx(), tailwind()]
 });
