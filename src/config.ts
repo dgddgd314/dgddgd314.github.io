580fca09012e88383924a95b1b11191215d9b20e
dgddgd314 <dgddgd314@gmail.com>
2026-08-19 13:34:00 +0900

    v1.2: mobile view update

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
 export const SITE_TITLE = 'dgddgd314';
 export const SITE_DESCRIPTION = 'dgddgd314 technical archive';
 export const GENERATE_SLUG_FROM_TITLE = true;
-export const TRANSITION_API = true;
